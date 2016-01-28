9d917def4c56bfcb7d006eadefca4ef3e08f2c7a
wilbur6689 <phill6689@yahoo.com>
2016-01-28 10:02:28 -0700

    1/28/2016 updated the main with all the tests

--- mountKabru/src/model/World.java
+++ mountKabru/src/model/World.java
@@ -14,4 +14,59 @@ import java.util.Objects;
  */
 public class World implements Serializable {
     
+    private char rowCount;
+    private byte columnCount;
+
+    public World() {
+    }
+
+    public char getRowCount() {
+        return rowCount;
+    }
+
+    public void setRowCount(char rowCount) {
+        this.rowCount = rowCount;
+    }
+
+    public byte getColumnCount() {
+        return columnCount;
+    }
+
+    public void setColumnCount(byte columnCount) {
+        this.columnCount = columnCount;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + this.rowCount;
+        hash = 67 * hash + this.columnCount;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final World other = (World) obj;
+        if (this.rowCount != other.rowCount) {
+            return false;
+        }
+        return this.columnCount == other.columnCount;
+    }
+
+    @Override
+    public String toString() {
+        return "World{" + "rowCount=" + rowCount + ", columnCount=" + columnCount + '}';
+    }
+    
+    
+    
 }

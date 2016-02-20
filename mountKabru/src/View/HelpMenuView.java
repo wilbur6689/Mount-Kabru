682d6d5b9df77029f37d4f04334761cd29746392
Phill <Phill@Gamer-PC>
2016-02-19 18:41:52 -0700

    2/19/16 started the help menu view

--- mountKabru/src/View/HelpMenuView.java
+++ mountKabru/src/View/HelpMenuView.java
@@ -5,10 +5,107 @@
  */
 package View;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Phill
  */
 public class HelpMenuView {
+    private final String menu;
     
+    public HelpMenuView() {
+    this.menu = "\n"
+                + "\n--------------------------------------"
+                + "\n| Help Menu                          |"
+                + "\n--------------------------------------"
+                + "\nN - how to Start a new game"
+                + "\nL - help with the town"
+                + "\nS - help with fighting monsters"
+                + "\nH - help with leveling up"
+                + "\nQ - return to main menu"
+                + "\n--------------------------------------";
+    }
+    
+    void displayMainMenuView() {
+          
+       boolean done = false; //set flag to done
+       do {
+          //prompt for and get the players name
+          String menuOption = this.getMenuOption();
+          if (menuOption.toUpperCase().equals("Q"))  //Player wants to quit
+            return; //exit game 
+          
+          //do the requested action and display the next view
+          done = this.doAction(menuOption);
+          
+       } while (!done);
+    }
+    
+    private String promptMessage;
+
+    private String getMenuOption() {
+        
+    
+        Scanner keyboard = new Scanner(System.in); // get the infile for keyboard
+        String value = ""; // Value to be Returned
+        boolean valid = false; // Initialize to not valid
+
+        while (!valid) { // Loop while an invalid value is enter
+          System.out.println ("\n" + this.menu);
+
+          value = keyboard.nextLine(); //Get next line typed on keyboard
+          value = value.trim(); // trim off leading and trailing blanks
+
+          if (value.length() < 1){ //value is blank
+            System.out.println("\n Invalid value: Value can not be blank");
+            continue;
+          }
+          break; // end the loop
+
+        }
+      return value;
+    }
+
+    private boolean doAction(String choice) {
+        
+        choice = choice.toUpperCase(); //convert choice to upper case
+        
+        switch (choice) {
+            case "N": // create and start a new game
+                this.startNewGame();
+                break;
+            case "L": // load players game
+                this.loadGame();
+                break;
+            case "S": //save a players game
+                this.saveGame();
+                break;
+            case "H": //help menu
+                this.displayHelpMenu();
+                break;
+            default:
+                System.out.println("\n*** Invalid selection *** Try again");
+                
+                         
+        }
+        return false;
+    }
+
+    private void startNewGame() {
+        System.out.println("*** startNewGame function called ***");
+    }
+
+    private void loadGame() {
+        System.out.println("*** loadGame function called ***");
+    }
+
+    private void saveGame() {
+        System.out.println("*** saveGame function called ***");
+    }
+
+    private void displayHelpMenu() {
+        System.out.println("*** displayHelpMenu function called ***");
+    }
+
 }

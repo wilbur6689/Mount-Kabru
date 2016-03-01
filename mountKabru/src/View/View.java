ccf3596061d54c57ff3faa9763f8bcb2ddf5c5aa
badgero1234 <badgero1234@icloud.com>
2016-03-01 16:27:23 -0700

    // Orson C Badger Created View and ViewInterface.
    Date : 3-1-16

---
+++ mountKabru/src/View/View.java
@@ -0,0 +1,53 @@
+package view;
+import java.util.Scanner;
+
+
+	public abstract class View implements ViewInterface {
+
+		Scanner keyboard = new Scanner(System.in);
+		protected String displayMessage;
+
+		public View(String message) {
+			this.displayMessage = message;
+		}
+
+
+		@Override
+		public void display() {
+			String value = "";
+			boolean done =false;
+
+			do {
+				System.out.println(this.displayMessage);
+				value = this.getInput();
+				done = this.doAction(value);
+
+			}
+			while (!done);
+		}
+
+		@Override
+
+		public String getInput();
+			boolean valid = false;
+			String selection = null;
+
+			while (!valid) {
+				//get the value entered from the keyboard
+				selection = keyboard.nextLine();
+				selection = selection.trim();
+
+				if (selection.length() < 1) {//blank value entered
+					System.out.println("You must enter a value");
+					continue;
+
+				}
+
+				break;
+			}
+
+			return selection; //return the name
+
+
+	}
+

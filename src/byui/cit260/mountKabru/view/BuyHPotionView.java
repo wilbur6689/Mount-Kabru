d4af0eb40a2a6049d23ac7ca8438d71bf81b8b7c
Phill <Phill@Gamer-PC>
2016-03-09 06:31:18 -0700

    3/9/2016 home computer, commit before a pull

---
+++ src/byui/cit260/mountKabru/view/BuyHPotionView.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.mountKabru.view;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Orson C Badger
+ */
+public class BuyHPotionView extends View {
+
+
+    public BuyHPotionView() {
+        super( "\n"
+                + "\n--------------------------------------"
+                + "\n|  So it will be a HP Potion eh?     |"
+                + "\n|  We will see if I like ya enough   |"
+                + "\n--------------------------------------"
+                + "\nB - [B]uy Lvl 1-10 HP Potion"
+                + "\n"
+                + "\nQ - [Q]uit"
+                + "\n--------------------------------------");
+    }
+
+    
+
+   
+    @Override
+    public boolean doAction(String choice) {
+
+        choice = choice.toUpperCase(); //convert choice to upper case
+
+        switch (choice) {
+            case "B": // Buy Potion From TavernOwner
+                this.buyFromTavernOwner();
+                break;
+
+
+            default:
+                System.out.println("\n*** Invalid selection *** Try again");
+
+
+        }
+        return false;
+    }
+
+    private void buyFromTavernOwner() {
+        System.out.println("\n***   NOPE    ***"
+                +          "\n***   NOPE   ***"
+                +          "\n***   Please come back later.               ***");
+    }
+
+
+}
+    
+
---
+++ src/byui/cit260/mountKabru/view/StartProgramView.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.mountKabru.view;
+
+import byui.cit260.mountKabru.control.GameControl;
+import byui.cit260.mountKabru.model.Player;
+
+
+/**
+ *
+ * @author wibur
+ */
+public class StartProgramView extends View {
+
+    public StartProgramView() { 
+            super("\n"
+                + "\n" 
+                + "\n______________________________________________________" 
+                + "\n|****************************************************|" 
+                + "\n|    88     888                __                    |" 
+                + "\n|   88888  888888          ___/**i             88    |" 
+                + "\n|    88888888888          /*******i          88888   |" 
+                + "\n|   8888888888888        /*********i       88888888  |" 
+                + "\n|                       /           i                |" 
+                + "\n|             __       /             i___            |" 
+                + "\n|            /  i     /                  i           |" 
+                + "\n|           /    i___/                    i          |" 
+                + "\n|       ___/                               i______   |" 
+                + "\n|      /                                          i__|" 
+                + "\n|     /       ~~Welcome to Mount Kabru!!!!!~~        |" 
+                + "\n|    /  This game will challange your skills through |" 
+                + "\n|   /   patience, knowledge, and a little bit of luck|"  
+                + "\n|****************************************************|"
+                + "\n|----------------------------------------------------|"
+                + "\n"
+                + "\n"
+                + "\n Please enter your name: ");}
+   
+    @Override
+    public boolean doAction(String playersName) {
+        
+        if (playersName.length() < 2) {
+            System.out.println("\nInvalid players name: " 
+                    + "The name must be greater than one character in length");
+            return false;
+        
+        }
+    
+        // Call createPlayer() control function
+        Player player = GameControl.createPlayer(playersName);
+        
+        if (player == null) { //if unsuccessful
+            System. out.println("\nError creating the player.");
+            return false;
+        }
+        
+
+        this.displayNextView(player);
+        
+        return true;
+    }
+       
+    private void displayNextView(Player player) {
+        System.out.println("\n ====================================="
+                         + "\n Welcome to the game " + player.getName()
+                         + "\n We hope you have a lot of fun!"
+                         + "\n ====================================="
+                         );
+        
+        //Create MainMenuView object
+        MainMenuView mainMenuView = new MainMenuView();
+                
+        //Display the main menu view
+        mainMenuView.display();
+    }       
+
+    
+    
+   
+    
+    
+
+    public final void displayBanner() {
+         
+         
+        System.out.println(
+          "\n______________________________________________________"
+        + "\n|****************************************************|"
+        + "\n|    88     888                __                    |"
+        + "\n|   88888  888888          ___/**i             88    |"
+        + "\n|    88888888888          /*******i          88888   |"
+        + "\n|   8888888888888        /*********i       88888888  |"
+        + "\n|                       /           i                |"
+        + "\n|             __       /             i___            |"
+        + "\n|            /  i     /                  i           |"
+        + "\n|           /    i___/                    i          |"
+        + "\n|       ___/                               i______   |"
+        + "\n|      /                                          i__|"
+        + "\n|     /       ~~Welcome to Mount Kabru!!!!!~~        |"
+        + "\n|    /  This game will challange your skills through |"
+        + "\n|   /   patience, knowledge, and a little bit of luck|"  
+        + "\n|****************************************************|"
+        + "\n|----------------------------------------------------|" 
+        );
+    }
+
+    
+}

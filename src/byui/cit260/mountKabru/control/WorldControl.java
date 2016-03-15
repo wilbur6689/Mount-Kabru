f4f9d4d23fa93032f1043c87652a9b6479820b02
wilbur6689 <phill6689@yahoo.com>
2016-03-15 10:02:33 -0600

    3/14/2016 gworld control

--- src/byui/cit260/mountKabru/control/WorldControl.java
+++ src/byui/cit260/mountKabru/control/WorldControl.java
@@ -11,32 +11,32 @@ public class WorldControl {
 
     public World createWorld() {
         
-        World world = new World(20,20);
+        World world = new World(5,5);
         
         createEvents();
-        setWorldLocations();
+        setEventsToLocations();
                 
         return world;
     }
 
 
-    private static void setWorldLocations(World world) {
+    private static void setEventsToLocations(World world) {
       
         
         Location[][] locations = world.getLocations();
         
         
         //Start Point
-        locations[0][0].setLocations(locations[LocationType]);
-        locations[0][1].setLocations
-        locations[0][2].setLocations
-        locations[0][3].setLocations
-        locations[0][4].setLocations
-        locations[0][5].setLocations        
-        locations[0][6].setLocations
-        locations[0][7].setLocations
-        locations[0][8].setLocations
-        locations[0][9].setLocations
+        locations[0][0].setEvents(events[events.town]);
+        locations[0][1].setEvents(events[events.town]);
+        locations[0][2].setEvents(events[events.town]);
+        locations[0][3].setEvents(events[events.town]);
+        locations[0][4].setEvents(events[events.town]);
+        locations[0][5].setEvents(events[events.town]);        
+        locations[0][6].setEvents(events[events.town]);
+        locations[0][7].setEvents(events[events.town]);
+        locations[0][8].setEvents(events[events.town]);
+        locations[0][9].setEvents(events[events.town]);
         
         
         
@@ -63,31 +63,34 @@ public class WorldControl {
   
     
     private static Event[] createEvents() {
-        Event[] events = new Event[8];
+        Event[] events = new Event[9];
+        
+        Event townEvent = new Event("Town", "You return to Town!", "you look around for a place to shop");
+        events[0] = townEvent;
         
         Event plainsGoldEvent = new Event("Gold", "You Found Gold!", "you found the motherload");
-        events[0] = plainsGoldEvent;
+        events[1] = plainsGoldEvent;
         
         Event plainsMonsterEvent = new Event("Monster", "You Found a Monster!", "Prepare for Battle weakling");
-        events[1] = plainsMonsterEvent;
+        events[2] = plainsMonsterEvent;
         
         Event jungleGoldEvent = new Event("Gold", "You Found Gold!", "you found the motherload");
-        events[2] = jungleGoldEvent;
+        events[3] = jungleGoldEvent;
         
         Event jungleMonsterEvent = new Event("Monster", "You Found a Monster!", "Prepare for Battle weakling");
-        events[3] = jungleMonsterEvent;
+        events[4] = jungleMonsterEvent;
         
         Event forestGoldEvent = new Event("Gold", "You Found Gold!", "you found the motherload");
-        events[4] = forestGoldEvent;
+        events[5] = forestGoldEvent;
         
         Event forestMonsterEvent = new Event("Monster", "You Found a Monster!", "Prepare for Battle weakling");
-        events[5] = forestMonsterEvent;
+        events[6] = forestMonsterEvent;
         
         Event mountainGoldEvent = new Event("Gold", "You Found Gold!", "you found the motherload");
-        events[6] = mountainGoldEvent;
+        events[7] = mountainGoldEvent;
         
         Event mountainMonsterEvent = new Event("Monster", "You Found a Monster!", "Prepare for Battle weakling");
-        events[7] = mountainMonsterEvent;
+        events[8] = mountainMonsterEvent;
         
         
         return events;

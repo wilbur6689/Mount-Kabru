6752deb37fe35fc0528b756abe9c3d0384d18f14
Orson <badgero1234>
2016-03-23 08:51:10 -0600

    Coded the idea of Event Control
    Orson C Badger
    
    3-22-16

---
+++ src/byui/cit260/mountKabru/control/EventControl.java
@@ -0,0 +1,32 @@
+package byui.cit260.mountKabru.control;
+
+import byui.cit260.mountKabru.model.Event;
+
+package
+/**
+ * Created by badgero1234 on 3/23/16.
+ */
+public class EventControl {
+    public static String EventType(){
+    int EventType = 0;
+    
+    Event[] eventLocation = {
+        
+       Plains[]
+       Jungle[]
+       DarkForest[]
+       HighMountain[]
+        
+        
+    }
+
+
+
+
+
+
+
+
+
+    }
+}

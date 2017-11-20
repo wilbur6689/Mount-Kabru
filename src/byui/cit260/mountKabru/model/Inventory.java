2653d96a80d068efa2b793f5ccec113a1884e6d5
Phillip Willis <wilbur6689@gmail.com>
2017-11-20 10:26:31 -0500

    added a List of items to the inventory

--- src/byui/cit260/mountKabru/model/Inventory.java
+++ src/byui/cit260/mountKabru/model/Inventory.java
@@ -14,29 +14,20 @@ public class Inventory implements Serializable {
     private Item spellSlot;
     private Item healthPotionSlot;
     private Item manaPotionSlot;
-    
+    private List<Item> items;
     private Hero hero;
 
     public Inventory(){
     }
 
-    public Inventory(Item weaponSlot, Item armorSlot, Item spellSlot, Item healthPotionSlot, Item manaPotionSlot) {
+    public Inventory(Item weaponSlot, Item armorSlot, Item spellSlot, Item healthPotionSlot, Item manaPotionSlot, List<Item> items, Hero hero) {
         this.weaponSlot = weaponSlot;
         this.armorSlot = armorSlot;
         this.spellSlot = spellSlot;
         this.healthPotionSlot = healthPotionSlot;
         this.manaPotionSlot = manaPotionSlot;
-
-    }
-
-
-
-    public Item getSpellSlot() {
-        return spellSlot;
-    }
-
-    public void setSpellSlot(Item spellSlot) {
-        this.spellSlot = spellSlot;
+        this.items = items;
+        this.hero = hero;
     }
 
     public Item getWeaponSlot() {
@@ -55,6 +46,14 @@ public class Inventory implements Serializable {
         this.armorSlot = armorSlot;
     }
 
+    public Item getSpellSlot() {
+        return spellSlot;
+    }
+
+    public void setSpellSlot(Item spellSlot) {
+        this.spellSlot = spellSlot;
+    }
+
     public Item getHealthPotionSlot() {
         return healthPotionSlot;
     }
@@ -70,7 +69,14 @@ public class Inventory implements Serializable {
     public void setManaPotionSlot(Item manaPotionSlot) {
         this.manaPotionSlot = manaPotionSlot;
     }
-    
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
 
     public Hero getHero() {
         return hero;
@@ -80,52 +86,50 @@ public class Inventory implements Serializable {
         this.hero = hero;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 37 * hash + Objects.hashCode(this.weaponSlot);
-        hash = 37 * hash + Objects.hashCode(this.armorSlot);
-        hash = 37 * hash + Objects.hashCode(this.healthPotionSlot);
-        hash = 37 * hash + Objects.hashCode(this.manaPotionSlot);
-        return hash;
-    }
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
+        Inventory inventory = (Inventory) object;
+
+        if (weaponSlot != null ? !weaponSlot.equals(inventory.weaponSlot) : inventory.weaponSlot != null) return false;
+        if (armorSlot != null ? !armorSlot.equals(inventory.armorSlot) : inventory.armorSlot != null) return false;
+        if (spellSlot != null ? !spellSlot.equals(inventory.spellSlot) : inventory.spellSlot != null) return false;
+        if (healthPotionSlot != null ? !healthPotionSlot.equals(inventory.healthPotionSlot) : inventory.healthPotionSlot != null)
             return false;
-        }
-        if (getClass() != obj.getClass()) {
+        if (manaPotionSlot != null ? !manaPotionSlot.equals(inventory.manaPotionSlot) : inventory.manaPotionSlot != null)
             return false;
-        }
-        final Inventory other = (Inventory) obj;
-        if (this.weaponSlot != other.weaponSlot) {
-            return false;
-        }
-        if (this.armorSlot != other.armorSlot) {
-            return false;
-        }
-        if (this.healthPotionSlot != other.healthPotionSlot) {
-            return false;
-        }
-        if (this.manaPotionSlot != other.manaPotionSlot) {
-            return false;
-        }
+        if (items != null ? !items.equals(inventory.items) : inventory.items != null) return false;
+        if (hero != null ? !hero.equals(inventory.hero) : inventory.hero != null) return false;
+
         return true;
     }
-    
-    
 
-    @Override
-    public String toString() {
-        return "Inventory{" + "weaponSlot=" + weaponSlot + ", armorSlot=" + armorSlot + ", healthPotionSlot=" + healthPotionSlot + ", manaPotionSlot=" + manaPotionSlot + '}';
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (weaponSlot != null ? weaponSlot.hashCode() : 0);
+        result = 31 * result + (armorSlot != null ? armorSlot.hashCode() : 0);
+        result = 31 * result + (spellSlot != null ? spellSlot.hashCode() : 0);
+        result = 31 * result + (healthPotionSlot != null ? healthPotionSlot.hashCode() : 0);
+        result = 31 * result + (manaPotionSlot != null ? manaPotionSlot.hashCode() : 0);
+        result = 31 * result + (items != null ? items.hashCode() : 0);
+        result = 31 * result + (hero != null ? hero.hashCode() : 0);
+        return result;
     }
-    
-    
 
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Inventory{" +
+                "weaponSlot=" + weaponSlot +
+                ", armorSlot=" + armorSlot +
+                ", spellSlot=" + spellSlot +
+                ", healthPotionSlot=" + healthPotionSlot +
+                ", manaPotionSlot=" + manaPotionSlot +
+                ", items=" + items +
+                ", hero=" + hero +
+                '}';
+    }
 }
 
 
